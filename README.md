f79d653d4d233412038e2f343021add26648ec88
Otto Giron <ottog2486@gmail.com>
2018-02-27 03:28:01 -0600

    add string float32 float64 int32 int64

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # input
-Simple user input from stdin  go library
+Simple user input from stdin  go library. Will loop until a valid value is entered.
+
+## Available methods
+
+* input.GetString(label string)  string
+* input.GetInt32(label string)   int32
+* input.GetInt64(label string)   int64
+* input.GetFloat64(label string) float64
+* input.GetFloat32(label string) float32
--- input.go
+++ input.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,6 +21,66 @@ func GetString(label string) string {
 		val, err := reader.ReadString('\n')
 		if err == nil {
 			return strings.TrimSpace(val)
+		}
+	}
+}
+
+//GetInt32 asks the user for an int32 value
+func GetInt32(label string) int32 {
+	for {
+		fmt.Print(label, " ")
+		val, err := reader.ReadString('\n')
+		if err == nil {
+			val = strings.TrimSpace(val)
+			i, err := strconv.ParseInt(val, 10, 32)
+			if err == nil {
+				return int32(i)
+			}
+		}
+	}
+}
+
+//GetInt64 asks the user for an int32 value
+func GetInt64(label string) int64 {
+	for {
+		fmt.Print(label, " ")
+		val, err := reader.ReadString('\n')
+		if err == nil {
+			val = strings.TrimSpace(val)
+			i, err := strconv.ParseInt(val, 10, 64)
+			if err == nil {
+				return i
+			}
+		}
+	}
+}
+
+//GetFloat32 asks the user for an float32 value
+func GetFloat32(label string) float32 {
+	for {
+		fmt.Print(label, " ")
+		val, err := reader.ReadString('\n')
+		if err == nil {
+			val = strings.TrimSpace(val)
+			f, err := strconv.ParseFloat(val, 32)
+			if err == nil {
+				return float32(f)
+			}
+		}
+	}
+}
+
+//GetFloat64 asks the user for an float32 value
+func GetFloat64(label string) float64 {
+	for {
+		fmt.Print(label, " ")
+		val, err := reader.ReadString('\n')
+		if err == nil {
+			val = strings.TrimSpace(val)
+			f, err := strconv.ParseFloat(val, 64)
+			if err == nil {
+				return f
+			}
 		}
 	}
 }
